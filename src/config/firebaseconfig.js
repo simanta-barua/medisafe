f9df8963bcab2b0f7f0d3710ea09e13752ccd5eb
Simanta Barua <simanta.barua@yhaoo.com>
2021-10-18 01:25:12 +0600

    env.local added

---
+++ src/config/firebaseconfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey:process.env.REACT_APP_APIKEY,
+    authDomain:process.env.REACT_APP_AUTHDOMAIN,
+    projectId:process.env.REACT_APP_PROJECTID,
+    storageBucket:process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId:process.env.REACT_APP_MESSAGINGSENDERID,
+    appId:process.env.REACT_APP_APPID,
+
+};
+
+// Initialize Firebase
+const initializeAuthentication = () => {
+    return initializeApp(firebaseConfig)
+}
+
+export default initializeAuthentication;

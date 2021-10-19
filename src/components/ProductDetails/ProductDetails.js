95a5407194bfb19f892bdd4739022038f25d227a
Simanta Barua <simanta.barua@yhaoo.com>
2021-10-19 23:53:30 +0600

    detalis

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import { useParams } from 'react-router';
 import useFetch from '../../hooks/useFetch';
+import { Badge, Button, Col, Container, ListGroup, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const ProductDetails = () => {
     const { productid } = useParams();
     const [products] = useFetch();
     const newProduct = products[productid];
-    const {title} = newProduct;
+    const { title, image, price, details, category } = newProduct;
     return (
         <div>
-            <h5>{title}</h5>
+            <Container>
+                <h2>{title}</h2>
+                <Row className="text-box ">
+                    <Col className="text-center">
+                        <img src={image} alt="img" />
+                    </Col>
+                    <Col>
+                        <ListGroup variant="flush" >
+                            <ListGroup.Item> Category: <Badge pill bg="primary">{category}</Badge></ListGroup.Item>
+                            <ListGroup.Item>Price: ${price} </ListGroup.Item>
+
+                            <ListGroup.Item>Details: {details} </ListGroup.Item>
+                            <Link to="/shop" className="text-light p-3 m-3"> <Button variant="danger">View all Product </Button></Link>
+                        </ListGroup>
+                    </Col>
+                </Row>
+            </Container>
+
         </div>
     );
 };
